4310ef534a4771fc41431907bdc9fd2bdf338815
lipanpan-hub <47287977+lipanpan-hub@users.noreply.github.com>
2023-11-01 10:33:33 +0800

    注释一些不用的模型

--- app/constant.ts
+++ app/constant.ts
@@ -84,10 +84,10 @@ export const DEFAULT_MODELS = [
   //   name: "gpt-4-0314",
   //   available: false,
   // },
-  {
-    name: "gpt-4-0613",
-    available: true,
-  },
+  // {
+  //   name: "gpt-4-0613",
+  //   available: true,
+  // },
   // {
   //   name: "gpt-4-32k",
   //   available: false,
@@ -108,18 +108,18 @@ export const DEFAULT_MODELS = [
   //   name: "gpt-3.5-turbo-0301",
   //   available: false,
   // },
-  {
-    name: "gpt-3.5-turbo-0613",
-    available: true,
-  },
+  // {
+  //   name: "gpt-3.5-turbo-0613",
+  //   available: true,
+  // },
   {
     name: "gpt-3.5-turbo-16k",
     available: true,
   },
-  {
-    name: "gpt-3.5-turbo-16k-0613",
-    available: true,
-  },
+  // {
+  //   name: "gpt-3.5-turbo-16k-0613",
+  //   available: true,
+  // },
 ] as const;
 
 export const CHAT_PAGE_SIZE = 15;
